72245ff09666fa48748dba402badf34183f440ae
Justin Buth - Orange Coast Title Company <JustinButh@octitle.com>
2018-07-23 08:56:24 -0700

    Add project files.

---
+++ Vidly2/Controllers/CustomersController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Vidly2.Models;
+using Vidly2.ViewModels;
+
+namespace Vidly2.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if(customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerToEdit = _context.Customers.Where(cust => cust.Id == customer.Id).First();
+                customerToEdit.Name = customer.Name;
+                customerToEdit.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+                customerToEdit.MembershipTypeId = customer.MembershipTypeId;
+                customerToEdit.Birthdate = customer.Birthdate;
+
+            }
+            
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        // GET: Customers
+        //[Route("customers")]
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.Include(property => property.MembershipType).ToList();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.Include(property => property.MembershipType).SingleOrDefault(c => c.Id == id);
+
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+    }
+}
---
+++ Vidly2/Controllers/MoviesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+using Vidly2.ViewModels;
+
+namespace Vidly2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            var customers = new List<Customer>
+            {
+                new Customer{ Name = "Customer 1"},
+                new Customer{ Name = "Customer 2"},
+                new Customer{ Name = "Customer 1"},
+                new Customer{ Name = "Customer 1"},
+                new Customer{ Name = "Customer 1"},
+                new Customer{ Name = "Customer 1"},
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+
+            return View(viewModel);
+        }
+
+        // movies/released/year/month
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+    
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            var movies = _context.Movies.ToList();
+            var genres = new List<Genre>();
+
+
+            foreach (var movie in movies)
+            {
+                genres.Add(_context.Genres.Where(g => g.Id == movie.GenreId).FirstOrDefault());
+            }
+
+            var moviesWithGenres = new MoviesWithGenres
+            {
+                Movies = movies,
+                Genres = genres
+            };
+
+            return View(movies);
+
+
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id.HasValue)
+            {
+                var moviesWithGenres = _context.Movies.Include("Genre").ToList();
+                var selectedMovie = moviesWithGenres.Where(movie => movie.Id == id).FirstOrDefault();
+
+                return View(selectedMovie);
+            }
+            return Content("nothing");
+        }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("Movieform", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if(movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                movie.Genre = _context.Genres.Where(x => x.Id == movie.GenreId).First();
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieToUpdate = _context.Movies.Where(m => m.Id == movie.Id).First();
+                movieToUpdate.Name = movie.Name;
+                movieToUpdate.NumberInStock = movie.NumberInStock;
+                movieToUpdate.ReleaseDate = movie.ReleaseDate;
+                movieToUpdate.GenreId = movie.GenreId;
+                movieToUpdate.Genre = _context.Genres.Where(genre => genre.Id == movie.GenreId).First();
+            }
+            
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+    }
+}
