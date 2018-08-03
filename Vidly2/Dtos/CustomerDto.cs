5143f86f762ba864cb49b2fabcfb4bda07b0cee7
justinb <justinbuth91@live.com>
2018-08-03 16:51:43 -0700

    Auto mapper and finished customer api.

---
+++ Vidly2/Dtos/CustomerDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Vidly2.Models;
+
+namespace Vidly2.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        // Remove from Dto to completely decouple this from our domain objects
+        //public MembershipType MembershipType { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        //[Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
--- Vidly2/Models/Movie.cs
+++ Vidly2/Models/Movie.cs
@@ -27,6 +27,7 @@ namespace Vidly2.Models
         public DateTime ReleaseDate { get; set; }
 
         [Display(Name="Number in Stock")]
+        [Range(1, 20)]
         public byte NumberInStock { get; set; }
 
     }
