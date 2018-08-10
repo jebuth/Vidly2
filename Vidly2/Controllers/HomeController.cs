27e476a2dd0147d25ef7433f17e7c6b9636f4c4c
JustinButh <JustinButh91@live.com>
2018-08-10 11:14:31 -0700

    Output caching

--- Vidly2/Controllers/HomeController.cs
+++ Vidly2/Controllers/HomeController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace Vidly2.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        [OutputCache(Duration = 0, VaryByParam = "*", NoStore = true)]
         public ActionResult Index()
         {
             return View();
