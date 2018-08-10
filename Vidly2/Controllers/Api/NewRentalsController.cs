9e460cd530a80414c0ee5a2f6beaa028a1421317
JustinButh <JustinButh91@live.com>
2018-08-10 14:05:04 -0700

    Added Rental table with migration. Added Rentals api controller

---
+++ Vidly2/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly2.Dtos;
+
+namespace Vidly2.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
